b31e2acac760bc27032512d934ff719e1ebae853
Inna <i.yemelyanova@gmail.com>
2020-02-04 13:14:17 +0300

    rm old

--- lesson7/old/config/pagesInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    1 => "main.php",
-    2 => "users.php",
-    3 => "user.php",
-    4 => "addUser.php",
-    5 => "addUser_POST.php",
-];
--- lesson7/old/dz/lesson5/1/bd.php
+++
@@ -1,4 +0,0 @@
-<?
-$link = mysqli_connect('localhost', 'root', '', 'gbphp') or die(mysqli_error($link));
-
-define('DIR_IMG', 'images');
